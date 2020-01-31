30cb92dda9cc07dd6551a4edd213e65cb9c4cd99
viewdegree <viewdegree@163.com>
2020-01-31 21:26:18 +0800

    (优化)将主要的初始函数封装起来

--- 02-用fs和path模块生成前端项目/init_program.js
+++ 02-用fs和path模块生成前端项目/init_program.js
@@ -23,18 +23,23 @@ let  programeData = {
     ]
 }
 
-let {name,fileData} = programeData; //解构赋值
-if(!fs.existsSync(name)){
-    fs.mkdirSync(name);     //初始化文件夹
+function init_program(){
+    let {name,fileData} = programeData; //解构赋值
+    if(!fs.existsSync(name)){
+        fs.mkdirSync(name);     //初始化文件夹
 
-    fileData.forEach((value)=>{     //数组用forEach方法,里面的回调函数第一个参数是他的子项  
-            let dirname = path.join(name,value.name);           //相对路径
-            if(value.type==='dir'){         //创建文件夹
-                fs.mkdirSync(dirname);
-            }else{                          //写入空文件
-                fs.writeFile(dirname,'',(err)=>{console.log(err);});
-            }
-    })
-}else{
-    console.log("当前文件夹已存在");
+        fileData.forEach((value)=>{     //数组用forEach方法,里面的回调函数第一个参数是他的子项  
+                let dirname = path.join(name,value.name);           //相对路径
+                if(value.type==='dir'){         //创建文件夹
+                    fs.mkdirSync(dirname);
+                }else{                          //写入空文件
+                    fs.writeFile(dirname,'',(err)=>{console.log(err);});
+                }
+        })
+    }else{
+        console.log("当前文件夹已存在");
+    }
+
 }
+
+init_program();
