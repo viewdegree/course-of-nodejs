caff19e74252aa6b076db708f6ed79ac8b8c33f1
viewdegree <viewdegree@163.com>
2020-01-31 21:23:19 +0800

    (path)用path模块进行小优化

--- 02-用fs和path模块生成前端项目/init_program.js
+++ 02-用fs和path模块生成前端项目/init_program.js
@@ -1,5 +1,6 @@
 //初始化项目数据
 let fs = require('fs');
+let path = require('path');
 let  programeData = {
     name:'前端项目文件夹',      //项目文件夹名称
     fileData:[
@@ -27,7 +28,7 @@ if(!fs.existsSync(name)){
     fs.mkdirSync(name);     //初始化文件夹
 
     fileData.forEach((value)=>{     //数组用forEach方法,里面的回调函数第一个参数是他的子项  
-            let dirname = name+'/'+value.name;
+            let dirname = path.join(name,value.name);           //相对路径
             if(value.type==='dir'){         //创建文件夹
                 fs.mkdirSync(dirname);
             }else{                          //写入空文件
