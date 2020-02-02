d5e174882302a7c2cf7a9f03b910d61541cdcf53
viewdegree <viewdegree@163.com>
2020-02-02 11:29:53 +0800

    (modules)将控制层的计算分离到模型层 1.控制层的作用相当于包工头,用来不牵扯计算,只负责接收app.js文件的数据和传递数据 2.模型层的作用相当于干活的,用于计算,读写等复杂操作

--- 04-mvc案例因子计算器/controller/controller.js
+++ 04-mvc案例因子计算器/controller/controller.js
@@ -1,12 +1,12 @@
+/**
+ * controller层(控制层)的作用
+ * 1.不牵扯计算,只负责接收app.js文件的数据和传递数据
+ * 
+ */
+const math = require("../modules/math.js"); //获取数学的方程
 exports.showResult = function(req,res){
     console.log(req.params.number);
     let number = req.params.number;
-    let result = [];
-    //计算出对应的约束
-    for(let i=1;i<=number;i++){
-        if(number%i===0){
-            result.push(i)
-        }
-    }
+    let result = math(number);
     res.send(result);
 }
