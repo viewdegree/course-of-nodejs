0c9ae9849e4ae56c4e61f89c55db2f8330e2a10e
viewdegree <viewdegree@163.com>
2020-02-02 11:08:24 +0800

    (controller)将获取到的数字进行因数判断,将获取到的值保存到数组中

--- 04-mvc案例因子计算器/controller/controller.js
+++ 04-mvc案例因子计算器/controller/controller.js
@@ -1,5 +1,12 @@
 exports.showResult = function(req,res){
     console.log(req.params.number);
-    // res.send(req.params);
-    res.send(req.params.number);
+    let number = req.params.number;
+    let result = [];
+    //计算出对应的约束
+    for(let i=1;i<=number;i++){
+        if(number%i===0){
+            result.push(i)
+        }
+    }
+    res.send(result);
 }
